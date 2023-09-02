470f261466437c7274a13eef751e2c45d17f2a8b
SyedMdMizan <syedmizan19@gmail.com>
2023-09-02 14:50:29 +0530

    completed

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
+let input = document.getElementById("input");
+let inputbox = document.getElementById("inputvalue");
+let buttons = document.querySelectorAll(".button");
 let string = "";
-
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-    button.addEventListener('click', (e)=>{
-        if(e.target.innerHTML == '='){
-            string =  eval(string);
-            document.querySelector('input').value = string;
-
-        }
-        if(e.target.innerHTML == 'A'){
-            string =  "";
-            document.querySelector('input').value = string;
-
-        }
-        if(e.target.innerHTML == 'C'){
-            string =  string.substring(0,string.length-1);
-            document.querySelector('input').value = string;
-
-        }
-        else{
-            console.log(e.target);
-            string = string + e.target.innerHTML;
-            document.querySelector('input').value = string;
-        }
-    })
-})
+let ans = "";
+let arr = Array.from(buttons);
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      ans = eval(string);
+      input.value = ans;
+      inputbox.value = string;
+      string = ans;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+      inputbox.value = string;
+    } else if (e.target.innerHTML == "C") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      console.log(e.target);
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
